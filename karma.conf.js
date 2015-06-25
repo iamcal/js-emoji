584d35aff220f310e84612d2337b6a81a5a8b8aa
Cal Henderson <cal@iamcal.com>
2015-06-24 20:42:00 -0700

    added karma-coverage so we can generate coverage when running karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -27,14 +27,21 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+
     preprocessors: {
+      "lib/*.js": "coverage"
+    },
+
+    coverageReporter: {
+      type: "lcov",
+      dir: "coverage/"
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -65,7 +72,8 @@ module.exports = function(config) {
 
     plugins: [
       'karma-jasmine',
-      'karma-phantomjs-launcher'
+      'karma-phantomjs-launcher',
+      'karma-coverage'
     ]
   })
 }
