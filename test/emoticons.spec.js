5e4a92f5be6c2aa3f4297889b65d5f526f4cb4ce
Chris Montrois <cmontrois@slack-corp.com>
2016-04-01 16:12:38 -0700

    Add exceptions for emoticon-like text

--- test/emoticons.spec.js
+++ test/emoticons.spec.js
@@ -14,42 +14,71 @@ describe("Emoticons replacer", function(){
 	emoji.use_sheet = false;
 	emoji.avoid_ms_emoji = true;
 
-	it("replaces emoticons with markup", function(){
+	describe('replace_emoticons', function() {
 
-		// simple
-		expect(emoji.replace_emoticons(':)')).toBe(emoji_span('1f60a'));
+		it("replaces emoticons with markup", function(){
 
-		// multiple emoticons that map to a single emoji
-		expect(emoji.replace_emoticons(':p' )).toBe(emoji_span('1f61b'));
-		expect(emoji.replace_emoticons(':-p')).toBe(emoji_span('1f61b'));
-		expect(emoji.replace_emoticons(':P' )).toBe(emoji_span('1f61b'));
-		expect(emoji.replace_emoticons(':-P')).toBe(emoji_span('1f61b'));
-		expect(emoji.replace_emoticons(':b' )).toBe(emoji_span('1f61b'));
-		expect(emoji.replace_emoticons(':-b')).toBe(emoji_span('1f61b'));
+			// simple
+			expect(emoji.replace_emoticons(':)')).toBe(emoji_span('1f60a'));
 
-		// over-escaped emoticons
-		expect(emoji.replace_emoticons(':\\')).toBe(emoji_span('1f615'));
+			// multiple emoticons that map to a single emoji
+			expect(emoji.replace_emoticons(':p' )).toBe(emoji_span('1f61b'));
+			expect(emoji.replace_emoticons(':-p')).toBe(emoji_span('1f61b'));
+			expect(emoji.replace_emoticons(':P' )).toBe(emoji_span('1f61b'));
+			expect(emoji.replace_emoticons(':-P')).toBe(emoji_span('1f61b'));
+			expect(emoji.replace_emoticons(':b' )).toBe(emoji_span('1f61b'));
+			expect(emoji.replace_emoticons(':-b')).toBe(emoji_span('1f61b'));
+
+			// over-escaped emoticons
+			expect(emoji.replace_emoticons(':\\')).toBe(emoji_span('1f615'));
+		});
+
+		it("does not replace emoticon-like strings", function(){
+			var sunglasses = emoji_span('1f60e');
+			var blush = emoji_span('1f60a');
+
+			// look for parentheticals
+			expect(emoji.replace_emoticons('(foo 8)')).toBe('(foo 8)');
+			expect(emoji.replace_emoticons('8) () 8)')).toBe(sunglasses+' () '+sunglasses);
+			expect(emoji.replace_emoticons('8) ) 8)')).toBe(sunglasses+' ) '+sunglasses);
+			expect(emoji.replace_emoticons('8) ( 8)')).toBe(sunglasses+' ( 8)');
+			expect(emoji.replace_emoticons('(foo :) )')).toBe('(foo '+blush+' )');
+			expect(emoji.replace_emoticons('(foo 8) ) (foo :) )')).toBe('(foo '+sunglasses+' ) (foo '+blush+' )');
+
+			// numbered lists
+			expect(emoji.replace_emoticons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
+			expect(emoji.replace_emoticons('6) six\n7) seven\n8) eight')).toBe('6) six\n7) seven\n8) eight');
+			expect(emoji.replace_emoticons('6) six\n7) seven\n8) eight\n9) nine')).toBe('6) six\n7) seven\n8) eight\n9) nine');
+			expect(emoji.replace_emoticons('6) six\n8) eight')).toBe('6) six\n8) eight');
+		});
+
 	});
 
-	it("replaces emoticons with colons", function(){
+	describe('replace_emoticons_with_colons', function() {
 
-		expect(emoji.replace_emoticons_with_colons('i &lt;3 u')).toBe('i :heart: u');
-	});
+		it("replaces emoticons with colons", function(){
 
-	it("does not replace emoticon-like strings", function(){
-		// look for parentheticals
-		expect(emoji.replace_emoticons_with_colons('(foo 8)')).toBe('(foo 8)');
-		expect(emoji.replace_emoticons_with_colons('8) () 8)')).toBe(':sunglasses: () :sunglasses:');
-		expect(emoji.replace_emoticons_with_colons('8) ) 8)')).toBe(':sunglasses: ) :sunglasses:');
-		expect(emoji.replace_emoticons_with_colons('8) (: (foo 8) 8)')).toBe(':sunglasses: :blush: (foo 8) :sunglasses:');
-		expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :blush: )');
-		expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :blush: )');
+			expect(emoji.replace_emoticons_with_colons('i &lt;3 u')).toBe('i :heart: u');
+		});
 
-		// numbered lists
-		expect(emoji.replace_emoticons_with_colons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
-		expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight')).toBe('6) six\n7) seven\n8) eight');
-		expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight\n9) nine')).toBe('6) six\n7) seven\n8) eight\n9) nine');
-		expect(emoji.replace_emoticons_with_colons('6) six\n8) eight')).toBe('6) six\n8) eight');
+		it("does not replace emoticon-like strings", function(){
+
+			// look for parentheticals
+			expect(emoji.replace_emoticons_with_colons('(foo 8)')).toBe('(foo 8)');
+			expect(emoji.replace_emoticons_with_colons('8) () 8)')).toBe(':sunglasses: () :sunglasses:');
+			expect(emoji.replace_emoticons_with_colons('8) ) 8)')).toBe(':sunglasses: ) :sunglasses:');
+			expect(emoji.replace_emoticons_with_colons('8) ( 8)')).toBe(':sunglasses: ( 8)');
+			expect(emoji.replace_emoticons_with_colons('8) :) (foo 8) 8)')).toBe(':sunglasses: :blush: (foo 8) :sunglasses:');
+			expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :blush: )');
+			expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :blush: )');
+
+			// numbered lists
+			expect(emoji.replace_emoticons_with_colons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
+			expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight')).toBe('6) six\n7) seven\n8) eight');
+			expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight\n9) nine')).toBe('6) six\n7) seven\n8) eight\n9) nine');
+			expect(emoji.replace_emoticons_with_colons('6) six\n8) eight')).toBe('6) six\n8) eight');
+		});
+
 	});
 
 });
