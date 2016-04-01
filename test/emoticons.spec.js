f979df65486b1bf3b9e23809562b88d3b8dd0ff7
Chris Montrois <cmontrois@slack-corp.com>
2016-04-01 15:06:08 -0700

    Fix replace_emoticons_with_colons

--- test/emoticons.spec.js
+++ test/emoticons.spec.js
@@ -36,5 +36,21 @@ describe("Emoticons replacer", function(){
 		expect(emoji.replace_emoticons_with_colons('i &lt;3 u')).toBe('i :heart: u');
 	});
 
+	it("does not replace emoticon-like strings", function(){
+		// look for parentheticals
+		expect(emoji.replace_emoticons_with_colons('(foo 8)')).toBe('(foo 8)');
+		expect(emoji.replace_emoticons_with_colons('8) () 8)')).toBe(':sunglasses: () :sunglasses:');
+		expect(emoji.replace_emoticons_with_colons('8) ) 8)')).toBe(':sunglasses: ) :sunglasses:');
+		expect(emoji.replace_emoticons_with_colons('8) (: (foo 8) 8)')).toBe(':sunglasses: :blush: (foo 8) :sunglasses:');
+		expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :blush: )');
+		expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :blush: )');
+
+		// numbered lists
+		expect(emoji.replace_emoticons_with_colons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
+		expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight')).toBe('6) six\n7) seven\n8) eight');
+		expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight\n9) nine')).toBe('6) six\n7) seven\n8) eight\n9) nine');
+		expect(emoji.replace_emoticons_with_colons('6) six\n8) eight')).toBe('6) six\n8) eight');
+	});
+
 });
 
