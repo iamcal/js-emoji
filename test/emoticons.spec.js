e567bed15a291e7c711ac46decac3a762982f829
Cal Henderson <cal@iamcal.com>
2016-04-19 13:06:29 -0700

    Fix replace_emoticons_with_colons

--- test/emoticons.spec.js
+++ test/emoticons.spec.js
@@ -82,5 +82,21 @@ describe("Emoticons replacer", function(){
 
 	});
 
+	it("does not replace emoticon-like strings", function(){
+		// look for parentheticals
+		expect(emoji.replace_emoticons_with_colons('(foo 8)')).toBe('(foo 8)');
+		expect(emoji.replace_emoticons_with_colons('8) () 8)')).toBe(':sunglasses: () :sunglasses:');
+		expect(emoji.replace_emoticons_with_colons('8) ) 8)')).toBe(':sunglasses: ) :sunglasses:');
+		expect(emoji.replace_emoticons_with_colons('8) (: (foo 8) 8)')).toBe(':sunglasses: :blush: (foo 8) :sunglasses:');
+		expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :blush: )');
+		expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :blush: )');
+
+		// numbered lists
+		expect(emoji.replace_emoticons_with_colons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
+		expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight')).toBe('6) six\n7) seven\n8) eight');
+		expect(emoji.replace_emoticons_with_colons('6) six\n7) seven\n8) eight\n9) nine')).toBe('6) six\n7) seven\n8) eight\n9) nine');
+		expect(emoji.replace_emoticons_with_colons('6) six\n8) eight')).toBe('6) six\n8) eight');
+	});
+
 });
 
