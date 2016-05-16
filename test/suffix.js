773be5b24aac770487df28f5451b009a37687c38
Cal Henderson <cal@iamcal.com>
2016-05-16 15:25:07 -0500

    rebuild with tests

---
+++ test/suffix.js
@@ -0,0 +1,29 @@
+describe("Allows cache-busting suffixes", function(){
+
+	var emoji = new EmojiConvertor();
+
+	emoji.img_set = 'apple';
+	emoji.img_sets.apple.path = '/';
+	emoji.img_sets.apple.sheet = '/sheet.png';
+	emoji.colons_mode = false;
+	emoji.text_mode = false;
+	emoji.include_title = false;
+	emoji.allow_native = false;
+	emoji.avoid_ms_emoji = true;
+
+	emoji.init_env();
+	emoji.supports_css = false;
+	emoji.use_sheet = false;
+	emoji.use_css_imgs = false;
+
+	it("For individual images", function(){
+
+		emoji.img_suffix = '';
+		expect(emoji.replace_colons(':cloud:')).toBe('<img src="/2601.png" class="emoji" />');
+
+		emoji.img_suffix = '?FOO';
+		expect(emoji.replace_colons(':cloud:')).toBe('<img src="/2601.png?FOO" class="emoji" />');
+	});
+
+});
+
