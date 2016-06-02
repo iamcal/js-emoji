006904ded87b742e5a1ba98d1968787ecef11424
Cal Henderson <cal@iamcal.com>
2016-06-02 12:39:18 -0500

    switch tests to run against the minified source. this shows github issue #46 as failing

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'lib/emoji.js',
+      'lib/emoji.min.js',
       'test/*.js'
     ],
 
