6b1ad4054b3f0d833fdf36d074296371a68ecf96
Cal Henderson <cal@iamcal.com>
2016-06-02 12:41:40 -0500

    patch to make sure minification does not break regexes. found here: https://github.com/iamcal/js-emoji/issues/46

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,8 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        ASCIIOnly: true
       },
       build: {
         src: 'lib/emoji.js',
