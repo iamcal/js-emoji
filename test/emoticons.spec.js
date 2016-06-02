59fd3cc60e4c516912144c32f58c6f9991a69ef7
Cal Henderson <cal@iamcal.com>
2016-06-02 12:50:27 -0500

    updated the emoticon test for the new :) mapping old: U+1F60A / :blush: new: U+1F642 / :slightly_smiling_face:

--- test/emoticons.spec.js
+++ test/emoticons.spec.js
@@ -21,7 +21,7 @@ describe("Emoticons replacer", function(){
 		it("replaces emoticons with markup", function(){
 
 			// simple
-			expect(emoji.replace_emoticons(':)')).toBe(emoji_span('1f60a'));
+			expect(emoji.replace_emoticons(':)')).toBe(emoji_span('1f642'));
 
 			// multiple emoticons that map to a single emoji
 			expect(emoji.replace_emoticons(':p' )).toBe(emoji_span('1f61b'));
@@ -37,16 +37,16 @@ describe("Emoticons replacer", function(){
 
 		it("does not replace emoticon-like strings", function(){
 			var sunglasses = emoji_span('1f60e');
-			var blush = emoji_span('1f60a');
+			var slightly_smiling_face = emoji_span('1f642');
 
 			// look for parentheticals
 			expect(emoji.replace_emoticons('(foo 8)')).toBe('(foo 8)');
 			expect(emoji.replace_emoticons('8) () 8)')).toBe(sunglasses+' () '+sunglasses);
 			expect(emoji.replace_emoticons('8) ) 8)')).toBe(sunglasses+' ) '+sunglasses);
 			expect(emoji.replace_emoticons('8) ( 8)')).toBe(sunglasses+' ( 8)');
-			expect(emoji.replace_emoticons('8) :) (foo 8) 8)')).toBe(sunglasses+' '+blush+' (foo 8) '+sunglasses);
-			expect(emoji.replace_emoticons('(foo :) )')).toBe('(foo '+blush+' )');
-			expect(emoji.replace_emoticons('(foo 8) ) (foo :) )')).toBe('(foo '+sunglasses+' ) (foo '+blush+' )');
+			expect(emoji.replace_emoticons('8) :) (foo 8) 8)')).toBe(sunglasses+' '+slightly_smiling_face+' (foo 8) '+sunglasses);
+			expect(emoji.replace_emoticons('(foo :) )')).toBe('(foo '+slightly_smiling_face+' )');
+			expect(emoji.replace_emoticons('(foo 8) ) (foo :) )')).toBe('(foo '+sunglasses+' ) (foo '+slightly_smiling_face+' )');
 
 			// numbered lists
 			expect(emoji.replace_emoticons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
@@ -71,9 +71,9 @@ describe("Emoticons replacer", function(){
 			expect(emoji.replace_emoticons_with_colons('8) () 8)')).toBe(':sunglasses: () :sunglasses:');
 			expect(emoji.replace_emoticons_with_colons('8) ) 8)')).toBe(':sunglasses: ) :sunglasses:');
 			expect(emoji.replace_emoticons_with_colons('8) ( 8)')).toBe(':sunglasses: ( 8)');
-			expect(emoji.replace_emoticons_with_colons('8) :) (foo 8) 8)')).toBe(':sunglasses: :blush: (foo 8) :sunglasses:');
-			expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :blush: )');
-			expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :blush: )');
+			expect(emoji.replace_emoticons_with_colons('8) :) (foo 8) 8)')).toBe(':sunglasses: :slightly_smiling_face: (foo 8) :sunglasses:');
+			expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :slightly_smiling_face: )');
+			expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :slightly_smiling_face: )');
 
 			// numbered lists
 			expect(emoji.replace_emoticons_with_colons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
@@ -89,9 +89,9 @@ describe("Emoticons replacer", function(){
 		expect(emoji.replace_emoticons_with_colons('(foo 8)')).toBe('(foo 8)');
 		expect(emoji.replace_emoticons_with_colons('8) () 8)')).toBe(':sunglasses: () :sunglasses:');
 		expect(emoji.replace_emoticons_with_colons('8) ) 8)')).toBe(':sunglasses: ) :sunglasses:');
-		expect(emoji.replace_emoticons_with_colons('8) (: (foo 8) 8)')).toBe(':sunglasses: :blush: (foo 8) :sunglasses:');
-		expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :blush: )');
-		expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :blush: )');
+		expect(emoji.replace_emoticons_with_colons('8) (: (foo 8) 8)')).toBe(':sunglasses: :slightly_smiling_face: (foo 8) :sunglasses:');
+		expect(emoji.replace_emoticons_with_colons('(foo :) )')).toBe('(foo :slightly_smiling_face: )');
+		expect(emoji.replace_emoticons_with_colons('(foo 8) ) (foo :) )')).toBe('(foo :sunglasses: ) (foo :slightly_smiling_face: )');
 
 		// numbered lists
 		expect(emoji.replace_emoticons_with_colons('7) seven\n8) eight')).toBe('7) seven\n8) eight');
