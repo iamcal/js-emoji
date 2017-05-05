8b27d9ac2fc79d2c20fcb66b98ab8fe3f1d960dd
Cal Henderson <cal@iamcal.com>
2017-05-05 16:39:04 -0700

    moved emoji_unified() into the helper library

--- test/preamble.js
+++ test/preamble.js
@@ -10,3 +10,10 @@ function emoji_span(codepoint, text){
 	if (!text) text = '';
 	return '<span class="emoji emoji-sizer" style="background-image:url(/'+codepoint+'.png)" data-codepoints="'+codepoint+'">'+text+'</span>';
 }
+
+function emoji_unified(cp){
+	if (cp < 0x10000) return String.fromCharCode(cp);
+	var h = Math.floor((cp - 0x10000) / 0x400) + 0xD800;
+	var l = ((cp - 0x10000) % 0x400) + 0xDC00;
+	return String.fromCharCode(h) + String.fromCharCode(l);
+}
