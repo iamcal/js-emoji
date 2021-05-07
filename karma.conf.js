3f31bf30daf3905900e725698fdc4f305752e8df
Cal Henderson <cal@iamcal.com>
2021-05-07 17:46:49 +0000

    * switch to using recent package versions of karma/jasmine * switch from PahntomJS to ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,66 +1,26 @@
-// Karma configuration
-// Generated on Wed Jun 24 2015 19:44:32 GMT-0700 (PDT)
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
     files: [
       'test/preamble.js',
       'lib/emoji.min.js',
       'test/*.js'
     ],
-
-
-    // list of files to exclude
     exclude: [
     ],
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['story'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    browsers: ['ChromeHeadless'],
     singleRun: false,
-
     plugins: [
       'karma-jasmine',
-      'karma-phantomjs-launcher',
+      'karma-chrome-launcher',
       'karma-story-reporter',
     ]
   })
